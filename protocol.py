50920c37f74d9729980104f5802fc35103c221c9
alanlinzy <34940728+alanlinzy@users.noreply.github.com>
2019-11-15 22:51:58 +0800

    Update protocol.py

--- protocol.py
+++ protocol.py
@@ -23,30 +23,31 @@ logger = logging.getLogger("playground.__connector__." + __name__)
 class CrapPacketType(PacketType):
     DEFINITION_IDENTIFIER = "crap"
     DEFINITION_VERSION = "1.0"
-           
+
 class HandshakePacket(CrapPacketType):
     DEFINITION_IDENTIFIER = "crap.handshakepacket"
     DEFINITION_VERSION = "1.0"
+
     NOT_STARTED = 0
-    SUCCESS = 1
-    ERROR = 2
+    SUCCESS     = 1
+    ERROR       = 2
+
     FIELDS = [
         ("status", UINT8),
-        ("nonce", UINT32({Optional: True})),
-        ("nonceSignature", BUFFER({Optional: True})),
-        ("signature", BUFFER({Optional: True})),
-        ("pk", BUFFER({Optional: True})),
-        ("cert", BUFFER({Optional: True}))
+        ("nonce", UINT32({Optional:True})),
+        ("nonceSignature", BUFFER({Optional:True})),
+        ("signature", BUFFER({Optional:True})),
+        ("pk", BUFFER({Optional:True})),
+        ("cert", BUFFER({Optional:True}))
     ]
-
 class DataPacket(CrapPacketType):
     DEFINITION_IDENTIFIER = "crap.datapacket"
     DEFINITION_VERSION = "1.0"
+
     FIELDS = [
         ("data", BUFFER),
-        ("signature", BUFFER),
-        ("tag",BUFFER({Optional: True}))
-        ]
+        ("signature", BUFFER)
+    ]
 
 
 
