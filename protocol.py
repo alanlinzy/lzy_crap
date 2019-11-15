a06f76cb590626ed4f14344dd8bd22ff836a2b8c
alanlinzy <34940728+alanlinzy@users.noreply.github.com>
2019-11-15 23:33:11 +0800

    Update protocol.py

--- protocol.py
+++ protocol.py
@@ -57,7 +57,7 @@ class CRAPTransport(StackingTransport):
     def write(self,data):
         self.protocol.send_data(data)
     def close(self):
-        self.protocol.close()
+        self.protocol.connection_lost(None)
         #pass
     
 
