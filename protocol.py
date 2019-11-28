a2e8a2fb9cb252a0e45218ed89ff7087f0e2152a
alanlinzy <34940728+alanlinzy@users.noreply.github.com>
2019-11-29 01:05:46 +0800

    Update protocol.py

--- protocol.py
+++ protocol.py
@@ -20,6 +20,7 @@ from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.serialization import PublicFormat, Encoding, load_pem_public_key,load_pem_private_key
 from cryptography import x509
 from ..poop.protocol import POOP
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
@@ -394,30 +395,19 @@ class CRAP(StackingProtocol):
         
     def data_enc(self,data):#no
         print("enc")
-        encryptor = Cipher(
-            algorithms.AES(self.enc_key),
-            modes.GCM(self.iv),
-            backend=default_backend()
-        ).encryptor()
-        #encryptor.authenticate_additional_data(None)
-        ciphertext = encryptor.update(data) + encryptor.finalize()
-        self.transport.write(DataPacket(data=cipher_text).__serialize__())
+        scheme = AESGCM(self.enc_key)
+        ciphertext = scheme.encrypt(self.iv, data, None)
+        self.transport.write(DataPacket(data=ciphertext).__serialize__())
         self.iv = (int.from_bytes(self.iv, "big")+1).to_bytes(12,"big")#?
         print("fin enc")
     
     def data_dec(self,data):#no
         print("dec")
-        decryptor = Cipher(
-           algorithms.AES(self.dec_key),
-           modes.GCM(self.peer_iv, None),
-           backend=default_backend()
-        ).decryptor()
-        
-        #decryptor.authenticate_additional_data(None)
-        
+        scheme = AESGCM(self.dec_key)
+        plaintext = scheme.decrypt(self.peer_iv, data, None)
         self.peer_iv = (int.from_bytes(self.peer_iv, "big")+1).to_bytes(12,"big")
         print("fin dec")
-        return decryptor.update(data) + decryptor.finalize()
+        return plaintext
         
     def printpkt(self,pkt):  # try to print packet content
         print("--------------------")
@@ -431,51 +421,8 @@ class CRAP(StackingProtocol):
         self.higherProtocol().connection_lost(exc)
 
 
-        
+    
 
-                 
-
-
-
-
-def encrypt(key, plaintext, associated_data):
-    # Generate a random 96-bit IV.
-    iv = os.urandom(12)
-
-    # Construct an AES-GCM Cipher object with the given key and a
-    # randomly generated IV.
-    encryptor = Cipher(
-        algorithms.AES(key),
-        modes.GCM(iv),
-        backend=default_backend()
-    ).encryptor()
-
-    # associated_data will be authenticated but not encrypted,
-    # it must also be passed in on decryption.
-    encryptor.authenticate_additional_data(associated_data)
-
-    # Encrypt the plaintext and get the associated ciphertext.
-    # GCM does not require padding.
-    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
-
-    return (iv, ciphertext, encryptor.tag)
-
-def decrypt(key, associated_data, iv, ciphertext, tag):
-    # Construct a Cipher object, with the key, iv, and additionally the
-    # GCM tag used for authenticating the message.
-    decryptor = Cipher(
-        algorithms.AES(key),
-        modes.GCM(iv, tag),
-        backend=default_backend()
-    ).decryptor()
-
-    # We put associated_data back in or the tag will fail to verify
-    # when we finalize the decryptor.
-    decryptor.authenticate_additional_data(associated_data)
-
-    # Decryption gets us the authenticated plaintext.
-    # If the tag does not match an InvalidTag exception will be raised.
-    return decryptor.update(ciphertext) + decryptor.finalize()
 
 folder = "keyfiles/"
 
