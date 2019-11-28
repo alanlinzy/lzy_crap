6f48913456f80394ece856b8b458aee3e3e5cf1e
alanlinzy <34940728+alanlinzy@users.noreply.github.com>
2019-11-29 00:08:50 +0800

    Update protocol.py

--- protocol.py
+++ protocol.py
@@ -150,15 +150,15 @@ class CRAP(StackingProtocol):
     
         self.verification_key = self.signing_key.public_key()
         try:
-            with open(path + "/keyfile/team2_key.pem", "rb") as f:
+            with open(path + "/keyfiles/team2_key.pem", "rb") as f:
                 self.team2_signing_key = load_pem_private_key(f.read(), password=None, backend=default_backend())
                 self.team2_verification_key = self.signing_key.public_key()
                 self.team2_verification_key_bytes = self.team2_verification_key.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo)
-            with open(path + "/keyfile/20194_root_cert.pem", "rb") as f:
+            with open(path + "/keyfiles/20194_root_cert.pem", "rb") as f:
                 self.root_cert = x509.load_pem_x509_certificate(f.read(), default_backend())
                 self.root_public_key = self.root_cert.public_key()
                 
-            with open(path + "/keyfile/team2_cert.pem", "rb") as f:
+            with open(path + "/keyfiles/team2_cert.pem", "rb") as f:
                 self.team2_certification_bytes = f.read()
                 self.team2_cert = x509.load_pem_x509_certificate(self.team2_certification_bytes, default_backend())
         except Exception as e:
