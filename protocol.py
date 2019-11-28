e4430165250b0d37dbe649e3534da8ad830face8
alanlinzy <34940728+alanlinzy@users.noreply.github.com>
2019-11-28 23:41:10 +0800

    Update protocol.py

--- protocol.py
+++ protocol.py
@@ -316,7 +316,7 @@ class CRAP(StackingProtocol):
         for c in range(len(chain)):
             cert_to_verify = x509.load_pem_x509_certificate(chain[c], default_backend())
             try:
-                public_key.verify(cert_to_verify.signature, cert_to_verify.tbs_certificate_bytes, padding.PKCS1v15(), cert_to_verify.signature_hash_algorithm)
+                self.root_pubk.verify(cert_to_verify.signature, cert_to_verify.tbs_certificate_bytes, padding.PKCS1v15(), cert_to_verify.signature_hash_algorithm)
                 self.peer_root_verikey = cert_to_verify.public_key()
                 print("chain!")
                 return True
